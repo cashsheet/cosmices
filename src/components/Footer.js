0cd0bb614066d6da9c3fc0e62dccedcdb1d4c0b8
Mikkel Eriksen Klokkerud <mikkelek93@gmail.com>
2020-05-13 13:00:35 +0200

    Logo updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/try_on_logo2.png'
+import logo from '../img/logoOnlyCosmic.png'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
