4846add947bf021ef22a9a1cbe5af1520b22263d
Mikkel Eriksen Klokkerud <mikkelek93@gmail.com>
2020-05-13 14:46:06 +0200

    Remove Tagline from Header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import logo from '../img/SmallLogo.png'
+import logo from '../img/logoNoTagline.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
