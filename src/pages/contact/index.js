3713ab7948e87ce2ae3a78d042bbcf9a763d003c
seaxam <max@thepeopleagency.co>
2025-02-21 21:14:57 +0100

    Contact form fix

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,40 +1,83 @@
 import React from "react";
-import ReCAPTCHA from "react-google-recaptcha";
+import { navigate } from "gatsby-link";
 import * as styles from "../../components/NetlifyForm/NetlifyForm.module.css";
 import Layout from "../../components/Layout";
+import Recaptcha from "react-google-recaptcha";
 
-const Index = () => (
-	<>
+const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
+function encode(data) {
+	return Object.keys(data)
+		.map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+		.join("&");
+}
+export default class Index extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	handleChange = e => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
+
+	handleRecaptcha = value => {
+		this.setState({ "g-recaptcha-response": value });
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+		const form = e.target;
+		fetch("/", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: encode({
+				"form-name": form.getAttribute("name"),
+				...this.state
+			})
+		})
+			.then(() => navigate(form.getAttribute("action")))
+			.catch(error => alert(error));
+	};
+	render() {
+		return (
 		<Layout>
 			<section className="section">
 				<div className="container">
 					<div className="content">
-					<h1 className={styles.title}>Contact</h1>
+						<h1 className={styles.title}>Contact</h1>
 						<div className={styles.section}>
-							<p>
-								Please don't hesitate to reach out to us.
-							</p>
+							<p>Please don't hesitate to reach out to us.</p>
 							<form
 								name="Contact Form"
-								method="POST"
+								method="post"
 								data-netlify="true"
 								data-netlify-recaptcha="true"
 								action="/contact/thanks"
+								onSubmit={this.handleSubmit}
 							>
-								<input type="hidden" name="form-name" value="Contact Form" />
 								<div className="field">
-									<label className={styles.label}>Your Name:</label>
-									<input className={styles.field} type="text" name="name" />
+									<label className={styles.label}>
+										Your name:<br/>
+										<input className={styles.field} type="text" name="name" onChange={this.handleChange}/>
+									</label>
 								</div>
 								<div className="field">
-									<label className={styles.label}>Your Email:</label>
-									<input className={styles.field} type="email" name="email" />
+									<label className={styles.label}>
+										Your email:<br/>
+										<input className={styles.field} type="email" name="email" onChange={this.handleChange}/>
+									</label>
 								</div>
 								<div>
-									<label className={styles.label}>Message:</label>
-									<textarea className={styles.textarea} name="message"/>
+									<label className={styles.label}>
+										Message:<br/>
+										<textarea className={styles.textarea} name="message" onChange={this.handleChange}/>
+									</label>
 								</div>
-								<ReCAPTCHA sitekey={process.env.GATSBY_RECAPTCHA_KEY}/>
+								<Recaptcha
+									ref="recaptcha"
+									sitekey={RECAPTCHA_KEY}
+									onChange={this.handleRecaptcha}
+								/>
 								<button className="button is-link" type="submit">
 									Send
 								</button>
@@ -44,7 +87,6 @@ const Index = () => (
 				</div>
 			</section>
 		</Layout>
-	</>
-);
-
-export default Index;
+		);
+	}
+}
